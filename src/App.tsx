31f890417ef87b1f6360b9abbb9058e2f133510b
Eidler <eidlerdoc@gmail.com>
2021-03-26 09:52:17 -0300

    added a components folder, used export function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import {Button} from './components/Buttons'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Button />
+    </>
   );
 }
 
---
+++ src/components/Buttons.tsx
@@ -0,0 +1,5 @@
+export function Button() {
+    return(
+        <button type="button" >Botão: <strong>Click Me</strong></button>
+    )
+}
